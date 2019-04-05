c72d8e7177ead859c4539a37db366f2194fc6788
Jay Parmar <jnparmar@ncsu.edu>
2019-04-04 23:35:09 -0400

    init2

--- server.py
+++ server.py
@@ -1,21 +1,57 @@
+import json
+import ibmiotf.application
+from time import sleep, time
+import numpy as np
+import pandas as pd
+from joblib import load
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
 import os
+
+
+def myCallback(cmd):
+    if cmd.event == "doorData":
+        payload = json.loads(cmd.payload)
+        # print(cmd.payload)
+        df = pd.read_json(payload, orient='records')
+        print(df)
+        estimate(svclassifier.predict(df))
+
+
+def estimate(l):
+    open = 0
+    close = 0
+    for num in l:
+        if num == 1:
+            open += 1
+        else:
+            close += 1
+
+    if open > close:
+        print("open")
+        myData = {'doorStatus': 'Open'}
+        client.publishEvent(
+            "door_sensor", "b827eb0acdd1", "doorStatus", "json", myData)
+    else:
+        print("close")
+        myData = {'doorStatus': 'Close'}
+        client.publishEvent(
+            "door_sensor", "b827eb0acdd1", "doorStatus", "json", myData)
+
+
+print("ahah")
+svclassifier = load('door_model.joblib')
+
+
+options = ibmiotf.application.ParseConfigFile("server.cfg")
+
+client = ibmiotf.application.Client(options)
+client.connect()
+client.deviceEventCallback = myCallback
+client.subscribeToDeviceEvents(event="doorData")
+
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
-
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
-
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
-
+    while True:
+        sleep(0.2)
+except ibmiotf.ConnectionException as e:
+    print(e)
